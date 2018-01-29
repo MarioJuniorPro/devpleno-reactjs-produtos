4474767958c754add5157d7c9f7241ef894c2e58
Mario Costa Junior <mario.junior.pro@gmail.com>
2018-01-28 23:00:46 -0200

    adding react-router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import Home from './Home'
+import Sobre from './Sobre'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Fragment>
+            <header>
+              <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className='container'>
+                  <Link className="navbar-brand" to="/">Gerenciador de Produtos</Link>
+                  <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                      <li className="nav-item active">
+                        <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="nav-link" to="/">Produtos</Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link className="nav-link" to="/sobre">Sobre</Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+            </header>
+            <main role="main" className="container">
+              <Route exact path='/' component={Home} />
+              <Route exact path='/sobre' component={Sobre} />
+              {/* <h1 className="mt-5">Sticky footer with fixed navbar</h1> */}
+              {/* <p className="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body &gt; .container</code>.</p>
+              <p>Back to <a href="../sticky-footer">the default sticky footer</a> minus the navbar.</p> */}
+            </main>
+        </Fragment>
+      </Router>
     );
   }
 }
